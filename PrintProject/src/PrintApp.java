342760ee1624cd060308df8140adff39248c4c79
Weichang Du <wdu@unb.ca>
2019-10-23 16:08:36 -0300

    version 2

--- PrintProject/src/PrintApp.java
+++ PrintProject/src/PrintApp.java
@@ -3,7 +3,7 @@ public class PrintApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Name: Weichang");
+		System.out.println("Last Name: Du");
 	}
 
 }
