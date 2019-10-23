3877b6857d2919e2a73a20c572a27e4ad9044f15
Weichang Du <wdu@unb.ca>
2019-10-23 15:26:29 -0300

    v2

--- HelloProject2/src/HelloApp.java
+++ HelloProject2/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Du!!");
+		System.out.println("Hello Weichang Du!!");
 	}
 
 }
