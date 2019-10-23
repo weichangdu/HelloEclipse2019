da64658711acdcf82cb58502177d2466bbd9ead8
Weichang Du <wdu@unb.ca>
2019-10-23 14:56:46 -0300

    Second version

--- HelloProject/src/HellpApp.java
+++ HelloProject/src/HellpApp.java
@@ -3,7 +3,7 @@ public class HellpApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello!!!");
+		System.out.println("Hello World!!!");
 	}
 
 }
