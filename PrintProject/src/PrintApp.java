6d86299bf51cde0a283012259faae8e953c2d8c5
Weichang Du <wdu@unb.ca>
2019-10-23 16:34:49 -0300

    version 4

--- PrintProject/src/PrintApp.java
+++ PrintProject/src/PrintApp.java
@@ -4,6 +4,7 @@ public class PrintApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Email: wdu@unb.ca");
+		System.out.println("Address: 5 Spruce Terrace");
 	}
 
 }
